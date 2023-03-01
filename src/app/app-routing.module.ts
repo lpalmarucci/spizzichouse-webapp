d537528aa606acbbf39d4689c70a293e31defab1
Luca Palmarucci <luca.palmarucci@orbyta.it>
2023-03-01 12:34:11 +0100

    setup authentication module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './app/modules/authentication/authentication.component';
 
 const routes: Routes = [
   {
@@ -7,7 +8,8 @@ const routes: Routes = [
     children: [
       {
         path: 'login',
-        pathMatch: 'full'
+        pathMatch: 'full',
+        component: AuthenticationComponent
       }
     ]
   }
